fb4e5a79c90916b088d6ba132bd7a5a773c1af72
Wemerson Jesus <wejesuss@gmail.com>
2020-05-14 22:14:41 -0300

    Adds blank space

--- urlShortener/routes.js
+++ urlShortener/routes.js
@@ -3,27 +3,45 @@ const mongoose = require('mongoose')
 const shortUrl = require('./app/models/shortUrl')
 const routes = express.Router()
 
-mongoose.connect('mongodb://localhost/shortUrls', {
+mongoose.connect('mongodb+srv://w-1-5-j-1-4-s:w-1-5-j-1-4-s@cluster0-9acsi.mongodb.net/shortUrls?retryWrites=true&w=majority', {
     useNewUrlParser: true, useUnifiedTopology: true
-}) 
-
-routes.get("/", async function (req, res) {
-    const shortUrls = await shortUrl.find()
-    return res.render("index", { shortUrls })
 })
 
+let lastUrl = ''
+
+routes.get("/", async function (req, res) {
+    const url = await shortUrl.findOne({ full: lastUrl })
+    lastUrl = ''
+    return res.render("index", { url })
+})
+
+routes.get("/shortUrls", async function (req,res) {
+    return res.redirect("/")
+})
 
 routes.post("/shortUrls", async function (req, res) {
-    await shortUrl.create({ full: req.body.fullUrl })
-    
-    return res.redirect('/')
+    lastUrl = req.body.fullUrl
+    const alreadyExist = await shortUrl.findOne({ full: lastUrl })
+
+    let results = null
+    if(!alreadyExist) {
+        try {
+            results = await shortUrl.create({ full: lastUrl })
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    if(results) lastUrl = results.full
+
+    return res.redirect("/")
 })
 
 routes.get("/:shortUrl", async function (req,res) {
     const ShortUrl = await shortUrl.findOne({ short: req.params.shortUrl })
     if (ShortUrl == null) return res.sendStatus(404)
 
-    res.redirect(ShortUrl.full)
+    return res.redirect(ShortUrl.full)
 })
 
 module.exports = routes
